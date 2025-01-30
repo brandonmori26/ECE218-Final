d6daf8782755953ccc633e913eba142b826879d3
amunns1929 <amunns1929@gmail.com>
2025-01-30 12:28:05 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,117 +1,183 @@
-
-//=====[Libraries]=============================================================
-
-#include "mbed.h"
 #include "arm_book_lib.h"
+#include "mbed.h"
+#define daylightMin 100
+#define duskMax 50
 
-//=====[Declaration and initialization of public global objects]===============
+// Driver seat occupancy sensor (provides contact closure when occupied);
+// Passenger seat occupancy sensor (provides contact closure when occupied);
+// Driver seatbelt sensor (provides contact closure when fastened);
+// Passenger seatbelt sensor (provides contact closure when fastened);
+// Ignition push button (provides contact closure to request ignition);
+DigitalIn driveSeatUsed(D1);
+DigitalIn passSeatUsed(D2);
+DigitalIn driveBelt(D6);
+DigitalIn passBelt(D7);
+DigitalIn ignition(D5);
+// Alarm buzzer (sounds when ignition is inhibited);
+// Green indicator LED (lights when ignition is enabled);
+//  Blue indicator LED (lights when the engine has started);
+DigitalInOut alarm(PE_10);
+DigitalOut green(LED1);
+DigitalOut blue(LED2);
+// Headlight mode selector (provides ON, OFF, or AUTO signals);
+// Light sensor (provides ambient light level signal);
+// Left low beam lamp, right low beam lamp
+DigitalIn headlightSelect();
+DigitalIn lightSensor();
+DigitalIn leftLamp();
+DigitalIn rightLamp();
 
-DigitalIn ignitionButton(D7); // Simulates the ignition button
-DigitalIn pSeatSens(D6);      // Passenger seat sensor
-DigitalIn dSeatSens(D5);      // Driver seat sensor
+driveSeatUsed.mode(PullDown);
+passSeatUsed.mode(PullDown);
+driveBelt.mode(PullDown);
+passBelt.mode(PullDown);
+ignition.mode(PullDown);
 
-DigitalIn pSeatBelt(D4);      // Passenger seatbelt switch
-DigitalIn dSeatBelt(D3);      // Driver seatbelt switch
+UnbufferedSerial system(USBTX, USBRX, 115200);
+/*
+Ignition Subsystem
+When the driver sits down, display the message,
+“Welcome to enhanced alarm system model 218-W25”.
+Indicate ignition enabled with the green LED only when
+both seats are occupied and both seatbelts are fastened.
+ When the ignition button is pushed:
+-        If the ignition enabled (green LED) is lit, then normal ignition
+occurs. Light the blue LED and extinguish the green LED. Display the message,
+“Engine started.”
+-        If the ignition is not enabled (green LED not lit), then ignition is
+inhibited. Sound the alarm buzzer; display the message, “Ignition inhibited” and
+display all the reasons why the ignition was inhibited: “Passenger seat not
+occupied,” “Driver seatbelt not fastened,” etc. Once the error messages are
+displayed, the system allows additional start attempts. (note this change from
+Project 1 requirements) Keep the engine running even if the driver or passenger
+should remove their seat belts or exit the vehicle. When the engine is running,
+stop the engine when the ignition button is pushed and then released. (note this
+change from Project 1 requirements)
 
-DigitalOut ignitionLed(LED1); // Green LED: Ignition enabled
-DigitalOut engineLed(LED2);   // Blue LED: Engine started
 
+Headlight subsystem
+If the engine is running, and the user selects: ON, turn on both low beam lamps;
+if they select OFF, turn off all lamps.
+If the user selects the AUTO position:
+When the light level is above a “daylight” level, switch headlights off after
+a delay of 2 seconds.
+When the light level is below a “dusk” level, switch headlights on after a delay
+of 1 second. When between these two levels, the headlights should hold their
+previous state. Turn off all headlights when the engine is off.
+*/
 
+void greenUpdate() {
+  // Light the green LED only when both seats are occupied and both
+  // seatbelts are fastened.
+  if (driveSeatUsed == ON && passSeatUsed == ON && driveBelt == ON &&
+      passBelt == ON) {
+    green = ON;
+  } else {
+    green = OFF;
+  }
+}
 
-DigitalInOut alarmBuzzer(PE_10); // Alarm Buzzer for inhibited ignition
+void ignitionStart() {
+  // -        If the green LED is lit, then normal ignition occurs.
+  //          Light the blue LED and extinguish the green LED. Display
+  //          the message, “Engine started.”
+  blue = ON;
+  green = OFF;
+  system.write("Engine Started\n", 16);
 
-BufferedSerial uartUsb(USBTX, USBRX, 115200); // UART for messages
+  while (ignition == false) {
+    ignitionRun()
+  }
+  // Keep the engine running even if the driver or passenger should remove their
+  // seat
+  // belts or exit the vehicle.
+  while (ignition == true) {
+  }
+  // When the engine is running, stop the engine when the ignition button is
+  // pushed and then released. (note this change from Project 1 requirements)
+  // Turn off all headlights when the engine is off.
+  rightLamp == OFF leftLamp == OFF blue = OFF;
+  green = OFF;
+  system.write("Engine Stopped\n", 16);
+}
 
-//=====[Declaration and initialization of public global variables]=============
-
-bool ignitionAttempted = false; // Tracks if the ignition button has been pressed
-
-//=====[Declarations (prototypes) of public functions]=========================
-
-void inputsInit();
-void outputsInit();
-
-void checkSystemState();
-void handleIgnition();
-//=====[Main function, the program entry point after power on or reset]========
-
-int main()
-{
-    inputsInit();
-    outputsInit();
-    while (true){
-        if (ignitionAttempted == false){
-            checkSystemState();
-            handleIgnition();
-        }
+void ignitionRun() {
+  // If the engine is running, and the user selects: ON, turn on both low beam
+  // lamps;
+  if (headlightSelect == ON) {
+    rightLamp == ON leftLamp == ON
+  } else if (headlightSelect ==
+             OFF) { // if they select OFF, turn off all lamps.
+    rightLamp == OFF leftLamp == OFF
+  } else if (headlightSelect ==
+             AUTO) { // If the user selects the AUTO position:
+    if (lightSensor >=
+        daylightMin) { // When the light level is above a “daylight” level,
+      // switch headlights off after a delay of 2 seconds.
+      delay(2000);
+      rightLamp == OFF;
+      leftLamp == OFF;
+    } else if (lightSensor <=
+               duskMax) { // When the light level is below a “dusk” level,
+      // switch headlights on after a delay of 1 second.
+      delay(1000);
+      rightLamp == ON leftLamp == ON
+      // When between these two levels, the headlights should hold their
+      // previous state.
     }
+  }
 }
-    //=====[Implementations of public functions]===================================
 
-void checkSystemState()
-{
-    while (dSeatSens == OFF){ }
-
-        uartUsb.write( "Welcome to enhanced alarm system model 218-W24\r\n", 48 );
-
-    while (ignitionButton == OFF) {
-        if (dSeatSens && pSeatSens && dSeatBelt && pSeatBelt) {
-            ignitionLed = ON; // Enable ignition (Green LED ON)
-        } else {
-            ignitionLed = OFF; // Disable ignition (Green LED OFF)
-        }
+void ignitionFail() {
+  // -        If the green LED is not lit, then ignition is inhibited.
+  // Sound the alarm buzzer;
+  alarm.output();
+  //          display the message, “Ignition inhibited” and
+  system.write("Ignition inhibited\n", 20);
+  //     display all
+  //          the reasons why the ignition was inhibited: “Passenger
+  //          seat not occupied,” “Driver seatbelt not fastened,” etc.
+  if (driveSeatUsed == OFF) {
+    system.write("Driver seat unoccupied\n", 24);
+  }
+  if (passSeatUsed == OFF) {
+    system.write("Passenger seat unoccupied\n", 27);
+  }
+  if (driveBelt == OFF) {
+    system.write("Driver seatbelt not fastened\n", 30);
+  }
+  if (passBelt == OFF) {
+    system.write("Passenger seatbelt not fastened\n", 31);
+  }
 }
-}
-void handleIgnition()
-{
-    if (ignitionLed == ON) {
-             // Normal ignition
-        ignitionLed = OFF; // Turn off the green LED
-        engineLed = ON;    // Turn on the blue LED
-        uartUsb.write("Engine started.\r\n", 17);
-        ignitionAttempted = true; //exit loop when ignition has been attempted
 
+int main() {
+  // Once the error messages are displayed, the system allows additional start
+  // attempts. (note this change from Project 1 requirements)
+  while (true) {
+    //   When the driver sits down, display the message,
+    // “Welcome to enhanced alarm system model 218-W25”.
+    while (driveSeatUsed == OFF) {
+    }
+    system.write("Welcome to enhanced alarm system model 218-W24\n", 47);
+    // Indicate ignition enabled with the green LED only when
+    // both seats are occupied and both seatbelts are fastened
+    while (ignition == OFF) {
+      greenUpdate()
+    }
+    //  When the ignition button is pushed:
+    // -        If the ignition enabled (green LED) is lit, then normal ignition
+    // occurs. Light the blue LED and extinguish the green LED. Display the
+    // message, “Engine started.”
+    if (green == ON) {
+      ignitionStart()
+      // -        If the ignition is not enabled (green LED not lit), then
+      // ignition is inhibited. Sound the alarm buzzer; display the message,
+      // “Ignition inhibited” and display all the reasons why the ignition was
+      // inhibited: “Passenger seat not occupied,” “Driver seatbelt not
+      // fastened,” etc.
     } else {
-                // Ignition inhibited
-
-                //sound alarm
-        alarmBuzzer.output();
-
-        uartUsb.write("Ignition inhibited.\r\n", 20);
-
-    if (!dSeatSens){
-            uartUsb.write("Driver seat not occupied\r\n", 27);
-            ignitionAttempted = true; //exit loop when ignition has been attempted
-
-    } if (!pSeatSens){
-        uartUsb.write("Passenger seat not occupied\r\n", 30);
-        ignitionAttempted = true; //exit loop when ignition has been attempted
-
-        } if (!dSeatBelt){
-            uartUsb.write("Driver seatbelt not fastened\r\n", 30);
-            ignitionAttempted = true; //exit loop when ignition has been attempted
-
-         } if (!pSeatBelt){
-            uartUsb.write("Passenger seatbelt not fastened\r\n", 33);
-            ignitionAttempted = true; //exit loop when ignition has been attempted
-        }
+      ignitionFail()
     }
-    }
-
-void inputsInit()
-            {
-            // Configure inputs as pull-down to ensure default state is LOW
-                ignitionButton.mode(PullDown);
-
-                pSeatSens.mode(PullDown);
-                dSeatSens.mode(PullDown);
-
-                alarmBuzzer.input();
-        //turns it off because when it's input it's not connected to ground
-            }
-void outputsInit()
-            {
-            // Initialize outputs to OFF
-                ignitionLed = OFF;
-                engineLed = OFF;
-            }
+  }
+}

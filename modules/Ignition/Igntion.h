316f87386e5f0c8f4fbd72c31618f6d77e7092c5
brandonmori26 <132004285+brandonmori26@users.noreply.github.com>
2025-02-11 14:47:34 -0500

    Update Igntion.h

--- modules/Ignition/Igntion.h
+++ modules/Ignition/Igntion.h
@@ -5,4 +5,7 @@ void outputsInit();
 void checkIgnitionConditions();
 void handleIgnitionButton();
 void displayInhibitReasons();
-
+//=========[Declarations of Private Variables]=========================
+bool engineRunning = false;           // Tracks if engine is running
+bool welcomeDisplayed = false;        // Tracks if welcome message has been shown
+bool previousButtonState = false;     // For detecting new button presses

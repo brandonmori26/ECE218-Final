d7ac84d25467c2f174e2de29e7c7c49ab840d51b
brandonmori26 <132004285+brandonmori26@users.noreply.github.com>
2025-02-11 15:49:34 -0500

    Update Igntion.h

--- modules/Ignition/Igntion.h
+++ modules/Ignition/Igntion.h
@@ -5,34 +5,6 @@
 
 //=====[Declarations (prototypes) of public functions]=========================
 bool ignitionUpdate()
-{
-    welcomeMessage();
-    ignitionEnable();
-    if (!engineStarted && debounceIgnition()){
-        uartUsb.write("Ignition attempted.\r\n\r\n", 23);
-        if (!greenLED){
-            sirenPin = BUZZER_ON;
-            errorMessage();
-            engineStarted = false;
-        }
-        else{
-            sirenPin = BUZZER_OFF;
-            greenLED = OFF;
-            blueLED = ON;
-            uartUsb.write("Engine started.\r\n\r\n", 19);
-            engineStarted = true;
-        }
-    }
-    else if (engineStarted && debounceIgnition()){
-        uartUsb.write("Engine off.\r\n\r\n", 15);
-        engineStarted = false;
-        blueLED = OFF;
-    }
-    
-    return engineStarted;
-}
-
-
 
 //=====[#include guards - end]=================================================
 

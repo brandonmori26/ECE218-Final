85a396febb939f8923a8628a496d36ed2d5de067
brandonmori26 <132004285+brandonmori26@users.noreply.github.com>
2025-02-11 14:39:51 -0500

    Update Igntion.h

--- modules/Ignition/Igntion.h
+++ modules/Ignition/Igntion.h
@@ -1,1 +1,8 @@
 
+//=====[Declarations (prototypes) of public functions]=========================
+void inputsInit();
+void outputsInit();
+void checkIgnitionConditions();
+void handleIgnitionButton();
+void displayInhibitReasons();
+

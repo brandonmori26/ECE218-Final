5c043fcdef6b22407c4db2fedbadaaf843ef01df
leoalchekw <weisberl@union.edu>
2025-02-13 21:22:30 +0000

    started windshield

--- modules/Windshield/Windshield.cpp
+++ modules/Windshield/Windshield.cpp
@@ -24,6 +24,25 @@ enum WiperMode {
 
 //=====[Declaration and initialization of private global variables]============
 
+#define PERIOD 0.02
+#define DUTY_MIN 0.025
+#define DUTY_MAX 0.125
+#define DUTY_67 0.0584
+
+#define LOW_SPEED_DELAY_30RPM 1250 // delay time for 30 RPM
+#define HIGH_SPEED_DELAY_40RPM 750 // delay time for 40 RPM
+
+#define WIPER_FULLRANGE_DELAY 370 //Time it takes for wiper to make full 67 degree rotation
+
+PwmOut servo(PF_9); //chargoggagoggmanchauggagoggchaubunagungamaugg
+
+void PwmInit();
+void PwmMax();
+void PwmMin();
+void Pwm67();
+void FullWipe();
+// main() runs in its own thread in the OS
+
 void PwmInit()
 {
     servo.period(PERIOD);
@@ -36,9 +55,15 @@ void PwmMax()
     servo.write(DUTY_MAX);
 }
 
-void PwmMin()
+void FullWipe()
 {
+    servo.write(DUTY_67);;
+    delay(370);
     servo.write(DUTY_MIN);
+    delay(370);
 }
+
+
+
 
 
